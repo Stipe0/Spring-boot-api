905e78755d0ef79d31eafb45ca46712e5f8a40c8
stipe0 <stipebabic9@gmail.com>
2020-08-14 15:05:44 +0200

    hashcode() equals()

--- src/main/java/com/example/demo/Model/User.java
+++ src/main/java/com/example/demo/Model/User.java
@@ -10,6 +10,16 @@ public class User {
 	private int id;
 	private String name;
 	private String job;
+	
+	public User() {
+		super();
+	}
+	public User(int id, String name, String job) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.job = job;
+	}
 	public String getName() {
 		return name;
 	}
@@ -31,6 +41,38 @@ public class User {
 	@Override
 	public String toString() {
 		return "User [name=" + name + ", job=" + job + ", id=" + id + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((job == null) ? 0 : job.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id != other.id)
+			return false;
+		if (job == null) {
+			if (other.job != null)
+				return false;
+		} else if (!job.equals(other.job))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
 	}
 
 }
